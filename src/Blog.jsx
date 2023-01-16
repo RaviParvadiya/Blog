731b11d2b282c5c57cde32e5592168624ddc6eea
RaviParvadiya <raviparvadiya.2001@gmail.com>
2023-01-16 10:27:23 +0530

    Update

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography, Box } from "@mui/material";
 import "./blog.css";
 
 function Blog() {
@@ -16,7 +16,7 @@ function Blog() {
       // console.log(response.data);
     };
     getPosts();
-  });
+  }, []);
 
   const Navigate = useNavigate();
 
@@ -28,10 +28,19 @@ function Blog() {
     <div>
       <Typography variant="h4">Blog</Typography>
       <div className="post-grid-container">
-        {posts.map((p) => {
-          return (
-            <Grid container spacing={2} minHeight={160} key={p.id}>
-              <Grid item xs={12} sm={9} key={p.id}>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)",
+            gridAutoRows: "minmax(100px, auto)"
+          }}
+        >
+          {posts.map((p) => {
+            return (
+              <Grid item xs={2} sm={4} md={4} key={p.id}>
                 <div>
                   <Typography variant="h4">{p.title}</Typography>
                   <Typography variant="subtitle2">By {p.writerName}</Typography>
@@ -44,9 +53,9 @@ function Blog() {
                   </Button>
                 </div>
               </Grid>
-            </Grid>
-          );
-        })}
+            );
+          })}
+        </Grid>
       </div>
     </div>
   );
